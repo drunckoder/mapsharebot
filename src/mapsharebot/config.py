c4831f18cdcf79eb9869e011360b673e4a4f8ee4
Aziz Anvarov <drunckoder@gmail.com>
2021-03-26 19:18:32 +0500

    added logging config

---
+++ src/mapsharebot/config.py
@@ -0,0 +1,7 @@
+import logging
+
+logging_config = {
+    'level': logging.DEBUG,
+    'format': '%(levelname)s :: %(asctime)s :: %(name)s :: %(funcName)s :: %(lineno)s :: %(message)s',
+    'datefmt': '%H:%M:%S'
+}
